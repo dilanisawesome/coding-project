a4ba7d1e541bc41109ecc3aeb3796c7d947b7717
dilanisawesome <83189859+dilanisawesome@users.noreply.github.com>
2023-02-24 13:51:39 -0700

    2 files modified

--- pages/start.js
+++ pages/start.js
@@ -1,9 +1,21 @@
 import Footer from '../components/Footer';
+import { useEffect, useState } from 'react';
 
 export default function Start() {
+  const [console, setConsole] = useState('');
+
   return (
     <main>
       <h1>Start</h1>
+      <button onClick={() => setConsole('NCAA')}>
+        <h3>Start in NCAA</h3>
+      </button>
+      <p>Console: {console}</p>
+      <br />
+      <button>
+        <h3>Start in Europe</h3>
+      </button>
+      <br />
       <Footer />
     </main>
   );
